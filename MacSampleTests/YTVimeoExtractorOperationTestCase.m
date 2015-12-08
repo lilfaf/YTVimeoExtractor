4f46d3593efe6c5e03fccfb7cb60e04273326d5f
Soneé John <sonee@alphasoftware.co>
2015-12-08 00:02:07 -0400

    Add YTVimeoExtractorOperationTestCase.m
    *Need to add test cases to the file*

---
+++ MacSampleTests/YTVimeoExtractorOperationTestCase.m
@@ -0,0 +1,29 @@
+//
+//  YTVimeoExtractorOperationTestCase.m
+//  Sample
+//
+//  Created by Soneé John on 12/7/15.
+//  Copyright © 2015 Louis Larpin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YTVimeoExtractorOperationTestCase : XCTestCase
+
+@end
+
+@implementation YTVimeoExtractorOperationTestCase
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+
+@end
