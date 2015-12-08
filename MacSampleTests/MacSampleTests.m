6985215f38f7a854cab14b991b0000d675050863
Soneé John <sonee@alphasoftware.co>
2015-12-07 23:54:13 -0400

    Remove "template" tests from `MacSampleTests.m`

--- MacSampleTests/MacSampleTests.m
+++ MacSampleTests/MacSampleTests.m
@@ -24,16 +24,4 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
