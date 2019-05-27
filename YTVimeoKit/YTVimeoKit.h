b8b2faa9adc6d2baaa7f871b369ca328d452a5ab
Soneé John <sonee@alphasoftware.co>
2019-05-27 18:57:47 -0400

    Add new initial project

---
+++ YTVimeoKit/YTVimeoKit.h
@@ -0,0 +1,19 @@
+//
+//  YTVimeoKit.h
+//  YTVimeoKit
+//
+//  Created by Soneé John on 5/27/19.
+//  Copyright © 2019 Louis Larpin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for YTVimeoKit.
+FOUNDATION_EXPORT double YTVimeoKitVersionNumber;
+
+//! Project version string for YTVimeoKit.
+FOUNDATION_EXPORT const unsigned char YTVimeoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YTVimeoKit/PublicHeader.h>
+
+
---
+++ YTVimeoKitTests/YTVimeoKitTests.swift
@@ -0,0 +1,34 @@
+//
+//  YTVimeoKitTests.swift
+//  YTVimeoKitTests
+//
+//  Created by Soneé John on 5/27/19.
+//  Copyright © 2019 Louis Larpin. All rights reserved.
+//
+
+import XCTest
+@testable import YTVimeoKit
+
+class YTVimeoKitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
