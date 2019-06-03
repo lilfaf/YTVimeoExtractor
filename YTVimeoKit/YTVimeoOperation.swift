aa776d113e57f5fcf20568fdb82c99a3367168dd
Soneé John <sonee@alphasoftware.co>
2019-06-03 10:17:00 -0400

    Complete configuration request & DownloadConfigurationResult

--- YTVimeoKit/YTVimeoOperation.swift
+++ YTVimeoKit/YTVimeoOperation.swift
@@ -36,6 +36,8 @@ import Foundation
     
     enum RequestType {
         case WatchPageRequest
+        case ConfigurationResult
+        case DownloadConfigurationResult
     }
     
     // MARK: -
@@ -47,6 +49,9 @@ import Foundation
     
     fileprivate var requestType: RequestType?
     fileprivate var webpage: YTVimeoVideoWebpage?
+    
+    fileprivate var configuration: [String : Any]?
+    fileprivate var downloadConfiguration: [String : Any]?
     
     //MARK: - Init
     
@@ -66,11 +71,15 @@ import Foundation
     }
     
     fileprivate func startReqest() {
-        requestType = .WatchPageRequest
-        
         var request = URLRequest(url: URL(string: "https://vimeo.com/\(videoIdentifier)")!)
         request.addValue("\(referer ?? "")", forHTTPHeaderField: "Referer")
         
+        self.request(request: request, requestType: .WatchPageRequest)
+    }
+    
+    fileprivate func request(request: URLRequest, requestType: RequestType) {
+        self.requestType = requestType
+       
         dataTask = session.dataTask(with: request, completionHandler: { (data, response, error) in
             self.handleData(data, response: response, error: error)
         })
@@ -83,10 +92,28 @@ import Foundation
         guard data != nil else { return }
         guard error == nil else { return }
         
+        if let httpResponse = response as? HTTPURLResponse {
+            guard httpResponse.statusCode == 200 else {
+                if self.requestType == .DownloadConfigurationResult {
+                    //Sometimes the download configuration will return 403
+                    //This is okay we simply work with the json that we have
+                    guard self.configuration != nil else { return }
+                    finishWithVideo(configuration: self.configuration!, downloadConfiguration: nil)
+                }
+                return
+            }
+        }
+        
         switch requestType {
         case .WatchPageRequest?:
             let html = String(decoding: data!, as: UTF8.self)
             handleWatchPageRequest(HTML: html)
+        case .DownloadConfigurationResult?:
+            guard let configuration = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String : Any] else { return }
+            handleDownloadConfigurationRequest(configuration: configuration)
+        case .ConfigurationResult?:
+            guard let configuration = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String : Any] else { return }
+            handleConfigurationResult(configuration: configuration)
         default:
             break
         }
@@ -95,7 +122,45 @@ import Foundation
     fileprivate func handleWatchPageRequest(HTML: String) {
         webpage = YTVimeoVideoWebpage(html: HTML)
         guard webpage?.vimeoConfiguration?["state"] as? String != "failed" else { return }
-        print(webpage?.configurationURL)
+        guard webpage != nil else { return }
+        guard webpage?.configurationURL != nil else { return }
         
+        startConfigurationRequest(url: webpage!.configurationURL!)
+    }
+    
+    fileprivate func startConfigurationRequest(url: URL) {
+        let request = URLRequest(url: url)
+        self.request(request: request, requestType: .ConfigurationResult)
+    }
+    
+    fileprivate func handleConfigurationResult(configuration: [String : Any]) {
+        self.configuration = configuration
+        startDownloadConfigurationRequest()
+    }
+    
+    fileprivate func startDownloadConfigurationRequest() {
+        guard var urlComponents = URLComponents(string: "https://vimeo.com/\(videoIdentifier)") else { return }
+        if #available(OSX 10.10, *) {
+            urlComponents.queryItems = [URLQueryItem(name: "action", value: "load_download_config")]
+        } else {
+            // Fallback on earlier versions
+        }
+        guard urlComponents.url != nil else { return }
+        
+        var request = URLRequest(url: urlComponents.url!)
+        request.addValue("XMLHttpRequest", forHTTPHeaderField: "X-Requested-With")
+        
+        self.request(request: request, requestType: .DownloadConfigurationResult)
+    }
+    
+    fileprivate func handleDownloadConfigurationRequest(configuration: [String : Any]) {
+        self.downloadConfiguration = configuration
+        
+        guard self.configuration != nil else { return }
+        finishWithVideo(configuration: self.configuration!, downloadConfiguration: self.downloadConfiguration)
+    }
+    
+    fileprivate func finishWithVideo(configuration: [String : Any], downloadConfiguration: [String : Any]?) {
+        let video = try! YTVimeoVideo(videoIdentifier: videoIdentifier, configuration: configuration, downloadConfiguration: downloadConfiguration)
     }
 }
