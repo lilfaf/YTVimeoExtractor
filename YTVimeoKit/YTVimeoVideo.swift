1707f683a043233ea07e7539c5fafe5d103660ad
Soneé John <sonee@alphasoftware.co>
2019-06-05 14:46:48 -0400

    Moved stream logic to new class YTVimeoVideoStream

--- YTVimeoKit/YTVimeoVideo.swift
+++ YTVimeoKit/YTVimeoVideo.swift
@@ -11,34 +11,14 @@ import Cocoa
 @objcMembers public class YTVimeoVideo: NSObject {
     public let title: String
     public let duration: TimeInterval
-    public var streamURLs: [UInt : URL]
-    public enum YTVimeoVideoQuality: UInt {
-        case low270 = 270
-        case medium360 = 360
-        case medium480 = 480
-        case medium540 = 540
-        case HD720 = 720
-        case HD1080p = 1080
-    }
+    public let stream: YTVimeoVideoStream
     
     internal init?(videoIdentifier: String, configuration: [String : Any], downloadConfiguration: [String : Any]?) throws {
         guard let videoInfo = configuration["video"] as? [String : Any] else { return nil }
-        guard let progressiveData = (configuration  as NSDictionary).value(forKeyPath: "request.files.progressive") as? [[String : Any]] else { return nil}
+        guard let stream = try? YTVimeoVideoStream(configuration: configuration, downloadConfiguration: downloadConfiguration) else { return nil }
         
+        self.stream = stream
         self.title = videoInfo["title"] as? String ?? ""
         self.duration = videoInfo["duration"] as? TimeInterval ?? 0
-        
-        var streamURLs: [UInt : URL] = [:]
-        
-        for info in progressiveData {
-            guard let height = info["height"] as? UInt else { continue }
-            guard let urlString = info["url"] as? String else { continue }
-            guard let url = URL(string: urlString) else { continue }
-            
-            streamURLs[height] = url
-        }
-        
-        guard streamURLs.isEmpty == false else { return nil }
-        self.streamURLs = streamURLs
     }
 }
---
+++ YTVimeoKit/YTVimeoVideoStream.swift
@@ -0,0 +1,49 @@
+//
+//  YTVimeoVideoStream.swift
+//  YTVimeoKit
+//
+//  Created by Soneé John on 6/5/19.
+//  Copyright © 2019 Louis Larpin. All rights reserved.
+//
+
+import Cocoa
+
+@objcMembers public class YTVimeoVideoStream: NSObject {
+    
+    public var sourceURL: URL? {
+        get {
+            guard let sourceURLString = (downloadConfiguration as NSDictionary?)?.value(forKeyPath: "source_file.download_url") as? String else { return nil }
+            return URL(string: sourceURLString)
+        }
+    }
+    
+    public var streamURLs: [YTVimeoVideoStreamQuality : URL]
+    public enum YTVimeoVideoStreamQuality: UInt {
+        case low270 = 270
+        case medium360 = 360
+        case medium480 = 480
+        case medium540 = 540
+        case HD720 = 720
+        case HD1080p = 1080
+    }
+    
+    fileprivate let downloadConfiguration: [String : Any]?
+
+    internal init?(configuration: [String : Any], downloadConfiguration: [String : Any]?) throws {
+        guard let progressiveData = (configuration  as NSDictionary).value(forKeyPath: "request.files.progressive") as? [[String : Any]] else { return nil }
+        
+        self.downloadConfiguration = downloadConfiguration
+        var streamURLs: [YTVimeoVideoStreamQuality : URL] = [:]
+        
+        for info in progressiveData {
+            guard let height = info["height"] as? YTVimeoVideoStreamQuality else { continue }
+            guard let urlString = info["url"] as? String else { continue }
+            guard let url = URL(string: urlString) else { continue }
+            
+            streamURLs[height] = url
+        }
+        
+        guard streamURLs.isEmpty == false else { return nil }
+        self.streamURLs = streamURLs
+    }
+}
